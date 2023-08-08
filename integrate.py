Integration normalization corrected for the 2D MC

integrate.py
@@ -168,7 +168,7 @@ def integrate_MC(calculation, x, y):
 
         evaluated_points_for_z_integration, evaluated_points_for_wz_integration = integrand(calculation)
         integral_over_z = normalization * np.sum(jacobian_for_z_integration * evaluated_points_for_z_integration) / no_of_samples
-        integral_over_wz = normalization * np.sum(jacobian_for_wz_integration * evaluated_points_for_wz_integration) / no_of_samples # TODO: Check by hand that there should not be normalization squared
+        integral_over_wz = normalization**2 * np.sum(jacobian_for_wz_integration * evaluated_points_for_wz_integration) / no_of_samples
         return integral_over_z + integral_over_wz
     else:
         if calculation['order_of_rk'] == 1:

tests/monte_carlo_nonuniform_sampling.py
@@ -50,7 +50,7 @@ def log_MC(log):
 
 
 def both_combined_the_Mareks_way():
-    no_of_samples = 1000000
+    no_of_samples = 100000
     steps_in_integrand_theta = 20
     integral = 0.
 
@@ -79,6 +79,41 @@ def both_combined_the_Mareks_way():
     print('True integral should be pi; MC:', integral)
 
 
+def both_combined_the_Mareks_way_2D():
+    no_of_samples = 100000
+    steps_in_integrand_theta = 20
+    integral = 0.
+
+    shift = 2. * np.pi / steps_in_integrand_theta / 2.  # to avoid double counting and y-axis with z and w.
+    integrand_angles = np.linspace(-np.pi + shift, np.pi - shift, steps_in_integrand_theta)
+    integrand_radii = np.logspace(-5., 1., 20000)
+    integrand_cartesian_coods = np.zeros((len(integrand_angles) * len(integrand_radii), 2))
+    for radius_ind in range(len(integrand_radii)):
+        for theta_ind in range(len(integrand_angles)):
+            x_cood = integrand_radii[radius_ind] * np.cos(integrand_angles[theta_ind])
+            y_cood = integrand_radii[radius_ind] * np.sin(integrand_angles[theta_ind])
+            integrand_cartesian_coods[radius_ind * len(integrand_angles) + theta_ind][0] = x_cood
+            integrand_cartesian_coods[radius_ind * len(integrand_angles) + theta_ind][1] = y_cood
+
+    random_indexes_z = np.random.randint(0, len(integrand_cartesian_coods), no_of_samples)  # z
+    random_indexes_w = np.random.randint(0, len(integrand_cartesian_coods), no_of_samples)  # z
+    array_of_z = integrand_cartesian_coods[random_indexes_z]
+    array_of_w = integrand_cartesian_coods[random_indexes_w]
+    # THE FUNCTION IS A UNIT FUNCTION
+    function_to_integrate = 1.
+    for integration_idx in range(no_of_samples):
+        integration_point_z = array_of_z[integration_idx]
+        integration_point_w = array_of_w[integration_idx]
+        jacobian_z = np.linalg.norm(integration_point_z) * np.log(10.) * np.linalg.norm(integration_point_z)  # One r for polar integration and ln(10)*r for log
+        jacobian_w = np.linalg.norm(integration_point_w) * np.log(10.) * np.linalg.norm(integration_point_w)  # One r for polar integration and ln(10)*r for log
+        integral += function_to_integrate*jacobian_z*jacobian_w
+    probability_normalization_polar = 2. * np.pi
+    probability_normalization_log = np.log10(integrand_radii[-1]) - np.log10(integrand_radii[0])
+    probability_normalization = probability_normalization_polar * probability_normalization_log
+
+    integral = integral * probability_normalization**2 / no_of_samples
+    print('True integral should be pi*100**2 = 98696; MC:', integral)
+
 
 # polar_MC(polar=True)
 # polar_MC(polar=False)
@@ -86,7 +121,8 @@ def both_combined_the_Mareks_way():
 # log_MC(log=True)
 # log_MC(log=False)
 
-both_combined_the_Mareks_way()
+# both_combined_the_Mareks_way()
+both_combined_the_Mareks_way_2D()
 
 
 