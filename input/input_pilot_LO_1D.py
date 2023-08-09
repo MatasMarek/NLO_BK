Bug in NLO found -> wrong sign in the second term after Kb

input/input_pilot_LO_1D.py
@@ -8,12 +8,12 @@ import os
 np.seterr(over='raise')
 # EDEBUG
 
-steps_in_integrand_theta = 20
+steps_in_integrand_theta = 200
 shift = 2.*np.pi/steps_in_integrand_theta/2.  # to avoid double counting and y-axis with z and w.
 grid = {
     'grid_in_Y': np.linspace(0., 10., 1001),
     'grid_in_r': np.logspace(-5., 2., 40),
-    'grid_in_integrand_radius': np.logspace(-5., 2., 200),
+    'grid_in_integrand_radius': np.logspace(-5., 2., 20000),
     'grid_in_integrand_angle': np.linspace(-np.pi + shift, np.pi - shift, steps_in_integrand_theta),  # not to include 2pi to avoid double counting
 }
 

input/input_pilot_NLO_1D.py
@@ -12,8 +12,8 @@ steps_in_integrand_theta = 20
 shift = 2.*np.pi/steps_in_integrand_theta/2.  # to avoid double counting and y-axis with z and w.
 grid = {
     'grid_in_Y': np.linspace(0., 10., 1001),
-    'grid_in_r': np.logspace(-5., 2., 40),
-    'grid_in_integrand_radius': np.logspace(-5., 2., 200),
+    'grid_in_r': np.logspace(-4., 2., 40),
+    'grid_in_integrand_radius': np.logspace(-7., 2., 20000),
     'grid_in_integrand_angle': np.linspace(-np.pi + shift, np.pi - shift, steps_in_integrand_theta),  # not to include 2pi to avoid double counting
 }
 
@@ -53,3 +53,4 @@ shutil.copyfile(__file__, '../output/' + run_name + '/input.py')
 shutil.copyfile('../const.py', '../output/' + run_name + '/const.py')
 
 run_calculation(calculation)
+

input/input_pilot_NLO_2D.py
@@ -11,7 +11,7 @@ grid = {
     'grid_in_Y': np.linspace(0., 10., 201),
     'grid_in_r': np.logspace(-7., 2., 50),
     'grid_in_b': np.logspace(-1., 2., 40),
-    'grid_in_integrand_radius': np.logspace(-5., 2., 200),
+    'grid_in_integrand_radius': np.logspace(-5., 2., 20000),
     'grid_in_integrand_angle': np.linspace(-np.pi + shift, np.pi - shift, steps_in_integrand_theta),  # not to include 2pi to avoid double counting
 }
 
@@ -22,7 +22,7 @@ integration_method = 'MC'
 no_of_samples = 10**6
 
 # DEBUG
-no_of_samples = 10**4
+no_of_samples = 10**5
 # EDEBUG
 
 order_of_rk = 1

integrate.py
@@ -66,12 +66,12 @@ def rs_bs_and_variables_for_N(calculation, x, y, no_of_samples, array_of_z=None)
             print('Simpson for NLO not implemented')
             exit(1)
         random_indexes_w = np.random.randint(0, len(integrand_cartesian_coods), no_of_samples)  # w
-
         # CONVERGENCE CONDITION: Fix that you dont have w and z the same since the integral then does not work
         same_elements_in_z_and_w = random_indexes_z == random_indexes_w
         while (same_elements_in_z_and_w).any():
             random_indexes_w[same_elements_in_z_and_w] = np.random.randint(0, len(integrand_cartesian_coods), len(random_indexes_w[same_elements_in_z_and_w]))
             same_elements_in_z_and_w = random_indexes_z == random_indexes_w
+
 
         array_of_w = integrand_cartesian_coods[random_indexes_w]
 
@@ -150,7 +150,6 @@ def integrate_MC(calculation, x, y):
     calculation = rs_bs_and_variables_for_N(calculation, x, y, no_of_samples)
     calculation = get_Ns(calculation)
 
-
     # Probability distribution normalization
     probability_normalization_polar = 2. * np.pi
     probability_normalization_log = np.log10(calculation['grid']['grid_in_integrand_radius'][-1]) - np.log10(calculation['grid']['grid_in_integrand_radius'][0])
@@ -167,8 +166,10 @@ def integrate_MC(calculation, x, y):
         jacobian_for_wz_integration = np.squeeze(jacobian_for_wz_integration)
 
         evaluated_points_for_z_integration, evaluated_points_for_wz_integration = integrand(calculation)
+
         integral_over_z = normalization * np.sum(jacobian_for_z_integration * evaluated_points_for_z_integration) / no_of_samples
         integral_over_wz = normalization**2 * np.sum(jacobian_for_wz_integration * evaluated_points_for_wz_integration) / no_of_samples
+
         return integral_over_z + integral_over_wz
     else:
         if calculation['order_of_rk'] == 1:

run.py
@@ -1,5 +1,5 @@
 import numpy as np
-from io_bk import print_out_calculation, save_calculation
+from io_bk import save_calculation
 from runge_kutta import runge_kutta
 import time
 from joblib import Parallel, delayed
@@ -119,10 +119,10 @@ def make_step(calculation):
     #  CONVERGENCE CONDITION
     # DEBUG CHECK FOR NEGATIVE VALUES IN N
     if (calculation['N'][y_ind] < 0.).any():
-        print(100.*np.sum(calculation['N'][y_ind] < 0.)/calculation['N'][y_ind].size, '% of negative values in N')
+        print(100.*np.sum(calculation['N'][y_ind] < 0.)/calculation['N'][y_ind].size, '% of negative values in N with a min of', np.min(calculation['N'][y_ind]))
         calculation['N'][y_ind][calculation['N'][y_ind] < 0.] = 0.
     if (calculation['N'][y_ind] > 1.).any():
-        print(100.*np.sum(calculation['N'][y_ind] > 1.)/calculation['N'][y_ind].size, ' % of values above 1 in N')
+        print(100.*np.sum(calculation['N'][y_ind] > 1.)/calculation['N'][y_ind].size, ' % of values above 1 in N with a max of', np.max(calculation['N'][y_ind]))
         calculation['N'][y_ind][calculation['N'][y_ind] > 1.] = 1.
     # DEBUG CHECK FOR NEGATIVE VALUES IN N
     return calculation
@@ -156,4 +156,6 @@ def run_calculation(calculation):
 #  * Get some convergence estimates for 2D ciBK
 #  * Map out the change in the proton for a small dipole size all bs and phis. Average over thetas.
 
+# It might very well be, that the unwanted unstable behavior for NLO comes from points when w and z are too close. Kernels diverge then.
+# TODO: I DID NOT HAVE ENOUGH POINTS IN INTEGRATION OVER r!, Change that in all inputs and add a check for that
 

tests/monte_carlo_nonuniform_sampling.py
@@ -80,13 +80,13 @@ def both_combined_the_Mareks_way():
 
 
 def both_combined_the_Mareks_way_2D():
-    no_of_samples = 100000
+    no_of_samples = 1000000
     steps_in_integrand_theta = 20
     integral = 0.
 
     shift = 2. * np.pi / steps_in_integrand_theta / 2.  # to avoid double counting and y-axis with z and w.
     integrand_angles = np.linspace(-np.pi + shift, np.pi - shift, steps_in_integrand_theta)
-    integrand_radii = np.logspace(-5., 1., 20000)
+    integrand_radii = np.logspace(-7., 2., 20000)
     integrand_cartesian_coods = np.zeros((len(integrand_angles) * len(integrand_radii), 2))
     for radius_ind in range(len(integrand_radii)):
         for theta_ind in range(len(integrand_angles)):
@@ -112,7 +112,7 @@ def both_combined_the_Mareks_way_2D():
     probability_normalization = probability_normalization_polar * probability_normalization_log
 
     integral = integral * probability_normalization**2 / no_of_samples
-    print('True integral should be pi*100**2 = 98696; MC:', integral)
+    print('True integral should be pi*10000**2 = 98696; MC:', integral/986960000.)
 
 
 # polar_MC(polar=True)